82bdd409bf0ba5195136fe7e242dd1df6ef90bbb
Kaelen-Tay-Kai-Gen <kaelentay01@gmail.com>
2023-09-22 12:14:29 +0800

    Input field HTML

--- d2w_mini_projects/mp_sort/app/static/__target__/org.transcrypt.__runtime__.js
+++ d2w_mini_projects/mp_sort/app/static/__target__/org.transcrypt.__runtime__.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2023-09-14 22:06:02
+// Transcrypt'ed from Python, 2023-09-22 12:12:01
 var __name__ = 'org.transcrypt.__runtime__';
 export var __envir__ = {};
 __envir__.interpreter_name = 'python';
